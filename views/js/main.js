9fff07fb810cc689dba9259779c61680ccc032d2
Dmitry <dkrupenya@gmail.com>
2015-11-04 14:21:27 +0300

    set fixed moving pizza size (no more resize events)

--- views/js/main.js
+++ views/js/main.js
@@ -530,9 +530,9 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.src = "images/pizza-w77.png";
+    //elem.style.height = "100px";
+    //elem.style.width = "77px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
